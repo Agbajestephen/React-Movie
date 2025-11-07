b7ee4b6ba3f271581ba4b32cf5610a89e020e646
Agbajestephen <agbajestephen5@gmail.com>
2025-11-07 15:50:52 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
-import "./App.css";
+import "./css/App.css";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
 
 function App() {
   const movieNumber = 2;
   return (
     <>
-    {/* ROUTES */}
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Favorites" element={<Favorites />} />
-        </Routes>
+      <div>
+        <NavBar />
+        {/* ROUTES */}
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Favorites" element={<Favorites />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
